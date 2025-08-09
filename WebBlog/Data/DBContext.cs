f1085296172e2a88dca32c867b9e188c2b3940d8
User <User@DESKTOP-CA0B49C>
2025-08-09 11:32:57 +0200

    Projektdateien hinzufügen.

---
+++ WebBlog/Data/DBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using WebBlog.Models.Domain;
+
+namespace WebBlog.Data
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPost { get; set; }
+        public DbSet<Tag> Tag { get; set; }
+    }
+}
---
+++ WebBlog/Models/Domain/BlogPost.cs
@@ -0,0 +1,18 @@
+﻿namespace WebBlog.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+        public string heading { get; set; }
+        public string PageTitle { get; set; }
+        public string Content { get; set; }
+        public string Description { get; set; }
+        public string FeaturedImgUrl { get; set; }
+        public string UrlHandle { get; set; }
+        public DateTime DoC { get; set; }
+        public string Author { get; set; }
+        public bool Visible { get; set; }
+        public ICollection<Tag> Tags { get; set; }
+
+    }
+}
