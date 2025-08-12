fe45ba6b594ba4193a5b993ff03578c8a7c2bd7e
Anouran1563 <anouarzinedine16@gmail.com>
2025-08-12 13:19:28 +0200

    Admin Functionality - Tags

--- WebBlog/Controllers/AdminTagsController.cs
+++ WebBlog/Controllers/AdminTagsController.cs
@@ -1,13 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebBlog.Data;
+using WebBlog.Models.Domain;
+using WebBlog.Models.ViewModels;
 
 namespace WebBlog.Controllers
 {
     public class AdminTagsController : Controller
     {
+        private readonly DBContext dBContext;
+        public AdminTagsController(DBContext dBContext)
+        {
+            this.dBContext = dBContext;
+        }
         [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            };
+            dBContext.Tag.Add(tag);
+            dBContext.SaveChanges();
+            return View("Add");
         }
     }
 }
